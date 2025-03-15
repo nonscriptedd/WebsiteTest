3919d28d4c5639184265ac32b12c0af4ef6a3c44
scriptedd_ <oguzturan0706@gmail.com>
2025-03-15 17:57:47 +0300

    Updated-Site

--- code/alert.js
+++ code/alert.js
@@ -1,2 +1,6 @@
 alert("Activate web-renderer64_48px?")
 console.log("This is an easter egg!")
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Page loaded');
+    // Add your JavaScript code here
+});
